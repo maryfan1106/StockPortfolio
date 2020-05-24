9fabaf9be62aac473a7109f9c5f2ef8acf1d829f
Mary Fan <maryfan1106@gmail.com>
2020-05-24 09:02:03 -0400

    added login action and save token in cookie

---
+++ client/src/actions/users.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export const logIn = (user, cb, errCb) => {
+  return axios
+  .post(`/users/login`, user)
+  // if successfully logged in, setAuth
+  .then((res) => Cookies.set("token", res.data.token))
+  .then(cb)
+  // return error to display as alert
+  .catch(err => errCb(err));
+};
